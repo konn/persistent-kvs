bd379ab2a21b2a16bb9b860c7c0db343fc783da1
Hiromi Ishii <konn.jinro_at_gmail.com>
2011-03-28 03:08:55 +0900

    first commit

---
+++ Database/Persist/Memcached.hs
@@ -0,0 +1,140 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Database.Memcached where
+import Blaze.ByteString.Builder
+import Data.ByteString hiding (take)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Database.Persist.Base
+import Data.Word
+import Data.Int
+import Data.Monoid
+import Data.Bits
+import Data.Fixed
+import Data.Time
+import Data.Attoparsec
+import Data.Attoparsec.Binary
+import Control.Applicative
+import Data.Ratio
+import Prelude hiding (take, foldr, length)
+import Network.Memcache
+import Network.Memcache.Serializable
+import Network.Memcache.Protocol
+import Control.Exception.Control
+import Control.Monad.IO.Control
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
+
+import Database.Persist
+
+instance Serializable PersistValue where
+  serialize = toByteString . buildPersistValue
+  deserialize = maybeResult . parse anyPersistValue
+
+newtype KVSPersist m a = KVSPersist { unKVSPersist :: ReaderT Server m a }
+  deriving (Functor, Monad, MonadIO, MonadControlIO)
+
+-- Strategy:
+-- values/[Entity Name]/[identifier]/[Field Name] : Field Value For Entity
+-- identifier/[Entity Name] : Fresh identifier for an Entity
+-- uniqs/[Entity Name]/[Unique Name]/[Values] : 
+--   Identifier table for an unique name.
+--   Values are separated by ":" after url encoded.
+
+instance (MonadControlIO m) => PersistBackend (KVSPersist m) where
+  insert val = do
+    let def = entityDef val
+    ident <- get
+
+buildPersistValue :: PersistValue -> Builder
+buildPersistValue (PersistString str) =
+  let bs = T.encodeUtf8 $ T.pack str
+  in fromWrite $ writeWord8 0 `mappend` writeWord64le (fromIntegral $ length bs)
+                              `mappend` writeByteString bs
+buildPersistValue (PersistByteString bs) =
+  fromWrite $ writeWord8 1 `mappend` writeWord64le (fromIntegral $ length bs)
+                           `mappend` writeByteString bs
+buildPersistValue (PersistInt64 i) =
+  fromWrite $ writeWord8 2 `mappend` writeInt64le i
+buildPersistValue (PersistDouble dbl) =
+  fromWrite (writeWord8 3) `mappend` fromDouble dbl
+buildPersistValue (PersistBool b) =
+  fromWrite $ writeWord8 4 `mappend` if b then writeWord8 1 else writeWord8 0
+buildPersistValue (PersistDay day) =
+  fromWrite (writeWord8 5) `mappend` buildInteger (toModifiedJulianDay day)
+buildPersistValue (PersistTimeOfDay (TimeOfDay a b c)) =
+  mconcat [ fromWrite $ writeWord8 6
+          , fromWrite $ writeInt64le (fromIntegral a)
+          , fromWrite $ writeInt64le (fromIntegral b)
+          , fromResolution c
+          ]
+buildPersistValue (PersistUTCTime (UTCTime a b)) =
+  mconcat [ fromWrite $ writeWord8 7
+          , buildInteger $ toModifiedJulianDay a
+          , fromInt64le $ floor $ toRational $ b * 10^12
+          ]
+buildPersistValue PersistNull = fromWord8 8
+
+fromDouble :: Double -> Builder
+fromDouble dbl =  buildInteger a `mappend` fromInt64le (fromIntegral b)
+  where (a, b) = decodeFloat dbl
+
+fromResolution :: (RealFrac (p a), HasResolution a) => p a -> Builder
+fromResolution a = buildInteger $ floor $ a * fromInteger (resolution a)
+
+buildInteger :: Integer -> Builder
+buildInteger n | lo <= n && n <= hi = fromWord8 0 `mappend` fromInt32le (fromIntegral n)
+  where
+    lo = fromIntegral (minBound :: Int32)
+    hi = fromIntegral (maxBound :: Int32)
+buildInteger n = fromWord8 1 `mappend` fromWord8 (fromIntegral $ signum n)
+                             `mappend` fromInt64le (fromIntegral $ length bts)
+                             `mappend` fromByteString bts
+  where
+    bts = unfoldr step $ abs n
+    step 0 = Nothing
+    step i = Just (fromIntegral i, i `shiftR` 8)
+
+anyPersistValue :: Parser PersistValue
+anyPersistValue = do
+  b <- anyWord8
+  case b of
+    0 -> PersistString . T.unpack . T.decodeUtf8 <$> (take . fromIntegral =<< anyWord64le)
+    1 -> PersistByteString <$> (take . fromIntegral =<< anyWord64le)
+    2 -> PersistInt64 <$> anyInt64le
+    3 -> PersistDouble <$> anyDouble
+    4 -> PersistBool True  <$ word8 1
+     <|> PersistBool False <$ word8 0
+    5 -> PersistDay . ModifiedJulianDay <$> anyInteger
+    6 -> PersistTimeOfDay <$> (TimeOfDay <$> (fromIntegral <$> anyInt64le)
+                                         <*> (fromIntegral <$> anyInt64le)
+                                         <*> anyPico)
+    7 -> PersistUTCTime <$> (UTCTime <$> (ModifiedJulianDay <$> anyInteger)
+                                     <*> ((/10^12). fromIntegral <$> anyInt64le))
+    8 -> pure PersistNull
+    _ -> fail $ "Unknown tag:" ++ show b
+
+anyInt64le :: Parser Int64
+anyInt64le = fromIntegral <$> anyWord64le
+
+anyInt32le :: Parser Int32
+anyInt32le = fromIntegral <$> anyWord32le
+
+anyInteger :: Parser Integer
+anyInteger = word8 0 *> (toInteger <$> anyInt32le)
+         <|> word8 1 *> ((*) . toInteger <$> anyInt8 <*> step)
+  where
+    step :: Parser Integer
+    step = foldr (flip $ (. fromIntegral) . (.|.) . (`shiftL` 8)) 0 <$> unroll
+    unroll :: Parser ByteString
+    unroll = anyInt64le >>= take . fromIntegral
+
+anyDouble :: Parser Double
+anyDouble = encodeFloat <$> anyInteger <*> (fromIntegral <$> anyInt64le)
+
+anyInt8 :: Parser Int8
+anyInt8 = fromIntegral <$> anyWord8
+
+anyPico :: Parser Pico
+anyPico = fromRational . (%10^12) <$> anyInteger
---
+++ test.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE TypeSynonymInstances, OverloadedStrings #-}
+module Main where
+import Test.QuickCheck
+import Database.Memcached
+import Data.Attoparsec
+import Blaze.ByteString.Builder
+import Control.Applicative
+import Database.Persist.Base
+import Data.ByteString
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.Time
+import Data.Fixed
+import Data.Ratio
+import Data.Maybe
+import Prelude hiding (length)
+import Data.Monoid
+import Data.Word
+import System.Random
+
+prop_persistSound :: PersistValue -> Bool
+prop_persistSound v = comp (anyPersistValue) buildPersistValue v == Just v
+
+prop_picoSound :: Pico -> Bool
+prop_picoSound v = comp anyPico fromResolution v == Just v
+
+prop_integerSound :: Property
+prop_integerSound = forAll (choose (0,2^35)) $ \v -> comp anyInteger buildInteger v == Just v
+
+prop_doubleSound :: Double -> Bool
+prop_doubleSound v = comp anyDouble fromDouble v == Just v
+
+prop_persistInvSound :: Property
+prop_persistInvSound = forAll arbitraryCode $ \bs -> 
+  let r = maybeResult $ parse (anyPersistValue) bs
+  in isJust r ==> maybe "" (toByteString . buildPersistValue) r == bs
+
+comp :: Parser a -> (a -> Builder) -> a -> Maybe a
+comp p b v = maybeResult $ flip feed "" $ parse p $ toByteString $ b v
+
+instance Arbitrary PersistValue where
+  arbitrary = do
+    num <- choose (0,8) :: Gen Int
+    case num of
+      0 -> PersistString <$> arbitrary
+      1 -> PersistByteString . T.encodeUtf8 . T.pack <$> arbitrary
+      2 -> PersistInt64 <$> arbitrary
+      3 -> PersistDouble <$> arbitrary
+      4 -> PersistBool <$> arbitrary
+      5 -> PersistDay <$> arbitraryDay 
+      6 -> PersistTimeOfDay <$> (TimeOfDay <$> arbitrary <*> arbitrary <*> arbitraryPico)
+      7 -> PersistUTCTime <$> (UTCTime <$> arbitraryDay <*> arbitraryDiffTime)
+      8 -> pure PersistNull
+
+arbitraryPico :: Gen Pico
+arbitraryPico = toEnum <$> arbitrary
+
+arbitraryDay :: Gen Day
+arbitraryDay = ModifiedJulianDay <$> arbitrary
+
+arbitraryDiffTime :: Gen DiffTime
+arbitraryDiffTime = toEnum <$> arbitrary
+
+instance Arbitrary Pico where
+  arbitrary = arbitraryPico
+
+instance Arbitrary ByteString where
+  arbitrary = T.encodeUtf8 . T.pack <$> listOf arbitrary
+
+arbitraryCode :: Gen ByteString
+arbitraryCode = do
+  tag <- choose (0::Int, 2)
+  case tag of
+    0 -> do
+      str <- arbitrary
+      let len = length str
+      return $ toByteString $ fromWord8 0 `mappend` fromWord64le (fromIntegral len) `mappend` fromByteString str
+    1 -> do
+      str <- arbitrary
+      let len = length str
+      return $ toByteString $ fromWord8 1 `mappend` fromWord64le (fromIntegral len) `mappend` fromByteString str
+    2 -> do
+      i64 <- arbitrary
+      return $ toByteString $ fromWord8 2 `mappend` fromInt64le i64
+    3 -> do
+      i <- arbitrary
+      b <- arbitrary :: Gen Int
+      return $ toByteString $ fromWord8 3 `mappend` buildInteger i `mappend` fromInt64le (fromIntegral b)
+    4 -> do
+      b <- choose (0,1)
+      return $ toByteString $ fromWord8 4 `mappend` fromWord8 (fromInteger b)
+    5 -> do
+      a <- arbitrary
+      return $ toByteString $ fromWord8 5 `mappend` buildInteger a 
+    6 -> do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitraryPico
+      return $ toByteString $ fromWrite (mconcat [writeWord8 6, writeWord64le a, writeWord64le b])
+                                `mappend` fromResolution c
+    7 -> do
+      a <- arbitrary
+      b <- arbitrary
+      return $ toByteString $ fromWord8 7 `mappend` buildInteger a `mappend` fromInt64le b
+    8 -> return $ toByteString $ fromWord8 8
